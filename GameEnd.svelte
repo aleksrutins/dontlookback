8659fd711def5b00ad8536f49ede914715ba650b
Aleks Rutins <aleks@rutins.com>
2022-05-16 10:20:11 -0400

    Add high score system

--- GameEnd.svelte
+++ GameEnd.svelte
@@ -1,6 +1,42 @@
 <script>
     export let score = 0;
+    let initials;
+    let highscores = JSON.parse(localStorage.getItem('dontlookback_highscores') || '{}');
+
+    $: localStorage.setItem('dontlookback_highscores', JSON.stringify(highscores));
+
+    function save() {
+        highscores[initials] = score;
+    }
 </script>
+<style>
+    div {
+        text-align: center;
+    }
+    table {
+        width: 50%;
+        margin: auto;
+        border: 1px solid black;
+        border-radius: 3px;
+        margin-top: 3px;
+    }
+</style>
 <div>
-    Your score: {score}
+    <h1>Your Score</h1>
+    {score}
+    <h1>Initials</h1>
+    <input type="text" bind:value={initials}>
+    <button on:click={save}>Save</button>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Score</th>
+        </tr>
+        {#each Object.keys(highscores).sort().reverse() as name}
+            <tr>
+                <td>{name}</td>
+                <td>{highscores[name]}</td>
+            </tr>
+        {/each}
+    </table>
 </div>
